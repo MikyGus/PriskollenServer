42677f14aba9e14afe463519207102f66050cf2c
MikyGus <mikygus@gmail.com>
2024-02-09 14:47:13 +0100

    Add project files.

---
+++ PriskollenServer/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace PriskollenServer;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
