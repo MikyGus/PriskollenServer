dd2198309f5187def3b516be780437d81544f64e
MikyGus <mikygus@gmail.com>
2024-02-12 21:46:43 +0100

    Add basic StoreChainController

---
+++ PriskollenServer/Controllers/StoreChainsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriskollenServer.Library.Contracts;
+
+namespace PriskollenServer.Controllers;
+
+[ApiController]
+public class StoreChainsController : ControllerBase
+{
+    [HttpPost("/storechains")]
+    public IActionResult CreateStoreChain(StoreChainRequest newStoreChain)
+    {
+        return Ok(newStoreChain);
+    }
+
+    [HttpGet("/storechains/{id:guid}")]
+    public IActionResult GetStoreChain(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpGet("/storechains/")]
+    public IActionResult GetStoreChain()
+    {
+        return Ok();
+    }
+
+    [HttpPut("/storechains/{id:guid}")]
+    public IActionResult UpdateStoreChain(Guid id, StoreChainRequest updatedStoreChain)
+    {
+        return Ok(id);
+    }
+
+}
