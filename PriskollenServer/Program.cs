cb380a688c8f4874042bd503a8b630e2cc58e4e2
MikyGus <mikygus@gmail.com>
2024-02-15 08:58:47 +0100

    Comment out Swagger

--- PriskollenServer/Program.cs
+++ PriskollenServer/Program.cs
@@ -25,8 +25,8 @@ try
     builder.Services.AddScoped<IStoreChainService, StoreChainService>();
 
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    //builder.Services.AddEndpointsApiExplorer();
+    //builder.Services.AddSwaggerGen();
 
     WebApplication app = builder.Build();
     {
@@ -34,13 +34,13 @@ try
         app.UseSerilogRequestLogging();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
         app.UseHttpsRedirection();
-        app.UseAuthorization();
+        //app.UseAuthorization();
         app.MapControllers();
         app.Run();
     }
