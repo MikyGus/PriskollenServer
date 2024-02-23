c377aa682c395d866b707ffb9c37cf5463c2ac70
MikyGus <mikygus@gmail.com>
2024-02-23 15:10:57 +0100

    Add store sql init-file

--- docker_database_init/02 - Store.sql
+++ docker_database_init/02 - Store.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE store (
+﻿CREATE TABLE stores (
 	id int auto_increment,
 	name varchar(40) not null,
 	image varchar(255),
@@ -11,25 +11,68 @@
 	primary key(id)
 	);
 
-INSERT INTO store (name, image, coordinate, address, city, storechain_id) VALUES
+INSERT INTO stores (name, image, coordinate, address, city, storechain_id) VALUES
 ('Ica Supermarket Hovshaga',null,Point(14.797456939889676,56.909225162189145),'Pomonavägen 6', 'Växjö',1),
 ('Ica Kvantum Norremark',null,Point(14.830311977284742,56.902149839127084),'Norremarksvägen 3', 'Växjö',1),
 ('Maxi Ica Stormarknad',null,Point(14.765980907577271,56.88369978800127),'Hejaregatan 30', 'Växjö',1);
 
 DELIMITER $$
 $$
-CREATE PROCEDURE GetAllStore()
+CREATE PROCEDURE CreateStore(
+	in Name varchar(40),
+	in Image varchar(40),
+	in Latitude double,
+	in Longitude double,
+	in address varchar(40),
+	in city varchar(40),
+	in storechain_id int)
 BEGIN
-	Select id, name, image, ST_AsText(coordinate), address, city, storechain_id, created, modified 
-	from store
+	INSERT INTO stores (name, image, coordinate, address, city, storechain_id) 
+	VALUES (Name, Image, Point(Longitude, Latitude), address, city, storechain_id)
+	RETURNING id, name, image, 
+		ST_AsText(coordinate), ST_X(coordinate) as longitude, ST_Y(coordinate) as latitude,
+		address, city, storechain_id, created, modified ;
+END$$
+
+CREATE PROCEDURE GetAllStores()
+BEGIN
+	Select id, name, image, 
+		ST_X(coordinate) as longitude, ST_Y(coordinate) as latitude,
+		address, city, storechain_id, created, modified 
+	from stores
 	order by name;
 END$$
-CREATE PROCEDURE GetAllStoreByDistance(in userlatitude double, in userlongitude double)
+
+CREATE PROCEDURE GetAllStoresByDistance(in userlatitude double, in userlongitude double)
 BEGIN
-	Select id, name, image, ST_AsText(coordinate) as coordinate, 
-	ST_DISTANCE_SPHERE(coordinate,Point(userlongitude, userlatitude))/1000 as distance, 
+	Select id, name, image, 
+		ST_X(coordinate) as longitude, ST_Y(coordinate) as latitude,
+		ST_DISTANCE_SPHERE(coordinate,Point(userlongitude, userlatitude))/1000 as distance, 
 	address, city, storechain_id, created, modified 
-	from store
+	from stores
 	order by distance;
 END$$
+
+CREATE PROCEDURE UpdateStore(
+	in SearchForId int,
+	in Name varchar(40),
+	in Image varchar(40),
+	in Latitude double,
+	in Longitude double,
+	in Address varchar(40),
+	in City varchar(40),
+	in Storechain_id int)
+BEGIN
+	UPDATE stores
+	SET name = Name, 
+		image = Image,
+		coordinate = Point(Longitude, Latitude),
+		address = Address,
+		city = City,
+		storechain_id = Storechain_id,
+		modified = CURRENT_TIMESTAMP()
+	WHERE id = SearchForId;
+	SELECT ROW_COUNT(); 
+END$$
+
 DELIMITER ;
