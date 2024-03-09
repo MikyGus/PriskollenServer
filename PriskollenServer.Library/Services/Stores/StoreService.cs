66fd8a73a9d288470edcf3bd5fead4c58d744537
MikyGus <mikygus@gmail.com>
2024-03-09 21:56:36 +0100

    Add StoreChain to GetAllStores

--- PriskollenServer.Library/Services/Stores/StoreService.cs
+++ PriskollenServer.Library/Services/Stores/StoreService.cs
@@ -57,43 +57,68 @@ public class StoreService : IStoreService
 
     public async Task<ErrorOr<List<Store>>> GetAllStores()
     {
-        string sqlString = @"Select id, name, image, 
- 		    ST_Y(coordinate) as latitude, ST_X(coordinate) as longitude,
-		    address, city, storechain_id, created, modified 
-	        from stores
-	        order by name;";
+        string sql = @"Select s.id, s.name, s.image, 
+           ST_Y(s.coordinate) as latitude, ST_X(s.coordinate) as longitude,
+          s.address, s.city, s.storechain_id, s.created, s.modified,
+                c.id, c.name, c.image, c.created, c.modified
+         from stores s
+            left join storechains c ON s.storechain_id = c.id;";
         var parameters = new { };
         try
         {
-            ErrorOr<List<Store>> result = await _dataAccess.LoadMultipleDataAsync<Store>(sqlString, parameters);
-            return result;
+            using IDbConnection connection = new MySqlConnection(_connectionString);
+            IEnumerable<Store> stores = await connection.QueryAsync<Store, StoreChain, Store>(sql, (store, storechain) =>
+            {
+                store.StoreChain = storechain;
+                return store;
+            }, param: parameters, splitOn: "Id");
+            if (stores is not null)
+            {
+                _logger.LogDebug("Successfully retreived stores {Store} with parameters {Parameters}", stores, parameters);
+                return stores.ToList();
+            }
+            _logger.LogWarning("Store not found with parameters {Parameters}", parameters);
+            return Errors.Store.NotFound;
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, "Failed to retreive Store from the database with parameters {Parameters} using sql {sql}",
-                parameters, sqlString);
+                parameters, sql);
             return Errors.Store.NotFound;
         }
     }
 
     public async Task<ErrorOr<List<Store>>> GetAllStoresByDistance(double latitude, double longitude)
     {
-        string sqlString = @"Select id, name, image, 
-		        ST_X(coordinate) as longitude, ST_Y(coordinate) as latitude,
-	            address, city, storechain_id, created, modified,
-		        ST_DISTANCE_SPHERE(coordinate,Point(@longitude, @latitude))/1000 as distance
-	        from stores
-	        order by distance;";
+        string sql = @"Select s.id, s.name, s.image, 
+           ST_Y(s.coordinate) as latitude, ST_X(s.coordinate) as longitude,
+          s.address, s.city, s.storechain_id, s.created, s.modified,
+          ST_DISTANCE_SPHERE(s.coordinate,Point(@longitude, @latitude))/1000 as distance,
+                c.id, c.name, c.image, c.created, c.modified
+         from stores s
+            left join storechains c ON s.storechain_id = c.id;";
+
         var parameters = new { latitude, longitude };
         try
         {
-            ErrorOr<List<Store>> result = await _dataAccess.LoadMultipleDataAsync<Store>(sqlString, parameters);
-            return result;
+            using IDbConnection connection = new MySqlConnection(_connectionString);
+            IEnumerable<Store> stores = await connection.QueryAsync<Store, StoreChain, Store>(sql, (store, storechain) =>
+            {
+                store.StoreChain = storechain;
+                return store;
+            }, param: parameters, splitOn: "Id");
+            if (stores is not null)
+            {
+                _logger.LogDebug("Successfully retreived stores {Store} with parameters {Parameters}", stores, parameters);
+                return stores.ToList();
+            }
+            _logger.LogWarning("Store not found with parameters {Parameters}", parameters);
+            return Errors.Store.NotFound;
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, "Failed to retreive Store from the database with parameters {Parameters} using sql {sql}",
-                parameters, sqlString);
+                parameters, sql);
             return Errors.Store.NotFound;
         }
     }
