ee1404b34fd470668efd294cb75cc2cb8daf9fa0
MikyGus <mikygus@gmail.com>
2024-03-09 14:36:03 +0100

    Add storeChain to CreateStore
    This is done by returning an Id from the insert
    and using this with GetStoreById

--- PriskollenServer.Library/Services/Stores/StoreService.cs
+++ PriskollenServer.Library/Services/Stores/StoreService.cs
@@ -29,18 +29,22 @@ public class StoreService : IStoreService
     {
         string sqlString = @"INSERT INTO stores (name, image, coordinate, address, city, storechain_id) 
 	        VALUES (@Name, @Image, Point(@Longitude, @Latitude), @address, @city, @storechain_id)
-	        RETURNING id, name, image, 
-		        ST_Y(coordinate) as latitude, ST_X(coordinate) as longitude,
-		        address, city, storechain_id, created, modified;";
+	        RETURNING id;";
         const string logErrorMessageTemplate = "Failed to create a new Store using parameters {Parameters}";
 
         try
         {
-            ErrorOr<Store> result = await _dataAccess.LoadSingleDataAsync<Store>(sqlString, store);
-            return result;
+            ErrorOr<int> newStoreId = await _dataAccess.LoadSingleDataAsync<int>(sqlString, store);
+            if (newStoreId.IsError == false)
+            {
+                ErrorOr<Store> newStore = await GetStore(newStoreId.Value);
+                return newStore;
+            }
+            return newStoreId.Errors;
         }
         catch (MySqlException ex)
         {
+            // TODO: Add if statement to check if it fails on StoreChainId being invalid
             _logger.LogError(ex, logErrorMessageTemplate, store);
             return Errors.Store.InvalidStoreChainId;
         }
@@ -96,16 +100,30 @@ public class StoreService : IStoreService
 
     public async Task<ErrorOr<Store>> GetStore(int id)
     {
-        string sql = @"Select id, name, image, 
- 		        ST_Y(coordinate) as latitude, ST_X(coordinate) as longitude,
-		        address, city, storechain_id, created, modified 
-	        from stores
-	        where id = @searchForId;";
+        string sql = @"Select s.id, s.name, s.image, 
+           ST_Y(s.coordinate) as latitude, ST_X(s.coordinate) as longitude,
+          s.address, s.city, s.storechain_id, s.created, s.modified,
+                c.id, c.name, c.image, c.created, c.modified
+         from stores s
+            left join storechains c ON s.storechain_id = c.id
+         where s.id = @searchForId;";
+
         var parameters = new { SearchForId = id };
         try
         {
-            ErrorOr<Store> result = await _dataAccess.LoadSingleDataAsync<Store>(sql, parameters);
-            return result;
+            using IDbConnection connection = new MySqlConnection(_connectionString);
+            IEnumerable<Store> store = await connection.QueryAsync<Store, StoreChain, Store>(sql, (store, storechain) =>
+            {
+                store.StoreChain = storechain;
+                return store;
+            }, param: parameters, splitOn: "Id");
+            if (store is not null)
+            {
+                _logger.LogDebug("Successfully retreived store {Store} with parameters {Parameters}", store, parameters);
+                return store.First();
+            }
+            _logger.LogWarning("Store not found with parameters {Parameters}", parameters);
+            return Errors.Store.NotFound;
         }
         catch (Exception ex)
         {
